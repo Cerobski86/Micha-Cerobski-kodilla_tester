2f74901d2f01e3a1f3faf4cb3906c6ef8f2ee658
Michał Cerobski <michalcerobski@tlen.pl>
2025-04-22 13:18:28 +0200

    Tablica 4.2

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -2,8 +2,6 @@ package com.kodilla.collections.arrays.homework;
 
 import com.kodilla.collections.interfaces.homework.Car;
 
-
-
 public class CarUtils {
     public static void describeCar(Car car) {
         System.out.println("---------------------------");
